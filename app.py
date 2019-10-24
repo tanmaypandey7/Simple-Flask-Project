3449051a1404b9e65a60efd782671e290036397c
tanmay <tanmaypandey1998@gmail.com>
2019-10-24 22:59:38 +0530

    renamed server.py to app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+# Import libraries
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)  # Load the model
+model = pickle.load(open('./model/model1.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the data from the POST request.
+    int_features = [int(x) for x in request.form.values()]
+    final_features = int_features
+    prediction = model.predict([np.array(final_features)])    
+    output = round(prediction[0])
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
