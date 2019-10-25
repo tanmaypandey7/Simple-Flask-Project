1e3677aaf031e341964065951825f76adb8497c4
tanmay <tanmaypandey1998@gmail.com>
2019-10-25 20:38:23 +0530

    updated app.py for PA

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
+import os
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)  # Load the model
-model = pickle.load(open('../model/model1.pkl', 'rb'))
+model = pickle.load(open(os.path.join(THIS_FOLDER, 'model/model1.pkl')))
 
 
 @app.route('/')
